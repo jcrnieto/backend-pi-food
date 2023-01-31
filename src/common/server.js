d1ada7eaae39d5bb7a6261f817a94f33215b3980
jcrnieto <82995532+jcrnieto@users.noreply.github.com>
2023-01-31 20:46:05 -0300

    common

--- src/common/server.js
+++ src/common/server.js
@@ -1,7 +1,5 @@
 const dotenv = require('dotenv');
-/*
-const NODE_ENV = process.env.NODE_ENV
-console.log('process.env.NODE_ENV', NODE_ENV);*/
+
 dotenv.config();
 //{ path:`./.env.${NODE_ENV}` }
 module.exports = {

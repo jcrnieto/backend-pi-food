1fa2498125ff746a14f057401c2fc13a851c5f04
jcrnieto <82995532+jcrnieto@users.noreply.github.com>
2023-01-31 10:33:35 -0300

    carpeta common

--- index.js
+++ index.js
@@ -19,10 +19,13 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const { SERVER_PORT } = require('./src/common/server')
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(SERVER_PORT, () => {
+    console.log(`%s listening at ${SERVER_PORT}`); // eslint-disable-line no-console
   });
 });
+
+server.timeout = 10000;
